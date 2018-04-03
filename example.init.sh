fd034dc656a204cda4df5e518daebd3b33e41a0b
Wilfried Bounsi <wilcoln99@gmail.com>
2018-04-03 13:32:12 +0100

    Creating the example.init.sh file

--- example.init.sh
+++ example.init.sh
@@ -3,11 +3,11 @@
 #   - copies all the files in the working folder
 #   - generates the application key
 #   - change the permission of the storage folder
-#   - run npm install to retrieve dependencies and then launch the server for live reload
+#   - run npm install to retrieve dependencies and the launches the server for live reload
 #   TODO: Change the destination folder from 'example' to your actual working folder.
 
-cp -Rf * /opt/lampp/htdocs/gallery/;
+cp -Rf * /opt/lampp/htdocs/example/;
 php artisan key:generate;
 echo 'Password required to change permissions on the storage folder'
-sudo chmod -R 777 /opt/lampp/htdocs/gallery/storage/
+sudo chmod -R 777 /opt/lampp/htdocs/example/storage/
 npm install && php artisan serve;
